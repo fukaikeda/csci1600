4d95bb16f9e36a9571f449a033ac5f7ec4a5847e
Jason Ni <jasonni@ucsb.edu>
2024-12-09 00:53:44 -0500

    GCal class API

---
+++ GCal2/gcal.h
@@ -0,0 +1,22 @@
+#ifndef GCAL_H
+#define GCAL_H
+
+#include <WiFiS3.h>
+#include <map>
+#include <Arduino.h>
+
+struct GCal {
+    const char* ssid;
+    const char* password;
+    const char* urlPath;
+    WiFiSSLClient sslClient;
+    std::map<String, String> homeTimes;
+
+    void connectWiFi();
+    void fetchData();
+    void printHomeTimes();
+    void followRedirect(const String& redirectURL);
+    String getHomeTime(String name);
+};
+
+#endif // GCAL_H
